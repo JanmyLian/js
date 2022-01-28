af2ce4d49690c522ddedd1442cf0a4707aa8dc1b
janmylian <569343390@qq.com>
2022-01-28 16:04:38 +0800

    1

--- 算法js/数组两数之和等于指定值.js
+++ 算法js/数组两数之和等于指定值.js
@@ -1,6 +1,9 @@
-function(arr, num){
+function getSum(arr, num){
+    if(Array.isArray(arr)){
+        return false;
+    }
     const len = arr.length;
-    let left = 0, right = len;
+    let left = 0, right = len - 1;
     for(let i = 0; i <= len; i++){
         if((arr[left] + arr[right]) > num){
             right--;
